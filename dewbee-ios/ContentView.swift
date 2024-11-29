8237d1bd3c6e153bfc5e5730ce854ec7ec62cef6
yeonjulee1005 <yeonju.lee1005@gmail.com>
2024-11-29 22:54:35 +0900

    불필요 소스 제거

--- dewbee-ios/ContentView.swift
+++ dewbee-ios/ContentView.swift
@@ -10,13 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         MyWebView(urlToLoad: "https://dewbee.dewdew.world")
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .ignoresSafeArea()
             .scrollIndicators(/*@START_MENU_TOKEN@*/.never/*@END_MENU_TOKEN@*/, axes: /*@START_MENU_TOKEN@*/[.vertical, .horizontal]/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
